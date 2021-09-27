09f02467a2fc12451b54755a9f5c23db4a7d1588
Bradley Travis <br4dl3y42@gmail.com>
2021-09-27 07:26:56 -0400

    -A

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function hello() {
 
 
 function game() {
+    let playerScore = 0;
+    let compScore = 0;
     function rps() {
         let playerChoice = prompt("Choose wisely...", "");
         let compChoice = ["rock", "paper", "scissors"][~~(Math.random() * 3)];
@@ -11,34 +13,43 @@ function game() {
             console.log("It's a tie...")
         }
         else if (playerChoice === "rock" && compChoice === "paper") {
-            console.log("Computer Wins, that means, YOU LOSE...")
+            console.log("Computer Wins, that means, YOU LOSE...");
+            compScore++;
         }
         else if (playerChoice === "rock" && compChoice === "scissors") {
             console.log("YOU WIN, Congrats!!!");
+            playerScore++;
         }
         else if (playerChoice === "scissors" && compChoice === "scissors") {
             console.log("It's a tie...")
         }
         else if (playerChoice === "scissors" && compChoice === "rock") {
-            console.log("Computer Wins, that means, YOU LOSE...")
+            console.log("Computer Wins, that means, YOU LOSE...");
+            compScore++;
         }
         else if (playerChoice === "scissors" && compChoice === "paper") {
             console.log("YOU WIN, Congrats!!!");
+            playerScore++;
         }
         else if (playerChoice === "paper" && compChoice === "paper") {
             console.log("It's a tie...")
         }
         else if (playerChoice === "paper" && compChoice === "scissors") {
-            console.log("Computer Wins, that means, YOU LOSE...")
+            console.log("Computer Wins, that means, YOU LOSE...");
+            compScore++;
         }
         else if (playerChoice === "paper" && compChoice === "rock") {
             console.log("YOU WIN, Congrats!!!");
+            playerScore++;
         }
         console.log(playerChoice);
         console.log(compChoice);
+        
     }
     rps();
     rps();
     rps();
-    
+    console.log('You won %s games', playerScore);
+    console.log('Computer won %s games', compScore);
+
 }
